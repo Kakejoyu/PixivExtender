7d5416651e8dd1cf1ba92f1f5255b84aca9920ea
Kakejoyu <kakejoyu@gmail.com>
2023-12-21 18:46:07 +0900

    Fix Pixiv_Extender.user.js
    - Fix code to add setting button.
    - Fix an issue with the incorrect style of buttons added in some pages.
    - Fix default selector for share button.

--- Pixiv_Extender.user.js
+++ Pixiv_Extender.user.js
@@ -4,7 +4,7 @@
 // @name:zh-CN  Pixiv Extender
 // @name:zh-TW  Pixiv Extender
 // @namespace   https://github.com/Kakejoyu/PixivExtender
-// @version     1.0.0
+// @version     1.0.1
 // @icon        https://www.pixiv.net/favicon.ico
 // @description "Pixiv Extender" is a user script derived from "Pixiv Plus" that adds various features to Pixiv. Thanks to Ahaochan, the developer of "Pixiv Plus"!
 // @description:ja    「Pixiv Extender」は、Pixivに様々な機能を追加する「Pixiv Plus」から派生したユーザースクリプトです。「Pixiv Plus」の開発者であるAhaochanに感謝します！
@@ -420,16 +420,16 @@ jQuery(($) => {
                 if (mutation.type !== "childList") {
                     continue;
                 }
-                if ($("#pe-setting-btn").length == 0) {
-                    $(mutation.target)
-                        .find(".sc-1rlbh8f-1.kvBUoX")
-                        .after(
-                            $(
-                                `<button id="pe-setting-btn" class="iahZRV"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill: currentColor; width: 20px;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg></button>`
-                            )
-                        );
-                    $("#pe-setting-btn").on("click", () => {
-                        $("body").append(`<div id="pe-bg">
+
+                $(mutation.target)
+                    .find(".sc-1rlbh8f-1.kvBUoX")
+                    .after(
+                        $(
+                            `<button title="${i18n(
+                                "setting_title"
+                            )}" id="pe-setting-btn" class="iahZRV ekdKos"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill: currentColor; width: 20px;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg></button>`
+                        ).on("click", () => {
+                            $("body").append(`<div id="pe-bg">
     <div id="pe-fg">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" id="pe-close"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
             <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"></path>
@@ -481,8 +481,8 @@ jQuery(($) => {
         <label class="pe-input-box">${i18n(
             "setting_downloadName"
         )}<input type="text" name="${
-                            GMkeys.downloadName
-                        }" placeholder="{pid}" /></label>
+                                GMkeys.downloadName
+                            }" placeholder="{pid}" /></label>
         <div class="pe-help-btn" onclick="alert('${i18n(
             "setting_name_description"
         )}')">?</div>
@@ -505,60 +505,66 @@ jQuery(($) => {
         #pe-fg div.pe-help-btn {display: inline-block;background: #0077ff;color: #ffffff;font-size: 15px;border-radius: 25px;text-align: center;width: 15px;height: 15px;margin-left: 5px;cursor: pointer;padding: 4px;vertical-align: middle;}
     </style>
 </div>`);
-                        $("#pe-bg").css({
-                            width: document.documentElement.clientWidth + "px",
-                            height:
-                                document.documentElement.clientHeight + "px",
-                        });
-                        let resizeTimer = null;
-                        $(window).on("resize", () => {
-                            if (resizeTimer != null) {
-                                clearTimeout(resizeTimer);
-                            }
-                            resizeTimer = setTimeout(() => {
-                                $("#pe-bg").css({
-                                    width:
-                                        document.documentElement.clientWidth +
-                                        "px",
-                                    height:
-                                        document.documentElement.clientHeight +
-                                        "px",
-                                });
-                            }, 500);
-                        });
-                        $("#pe-close").click(function () {
-                            $("#pe-bg").remove();
-                        });
+                            $("#pe-bg").css({
+                                width:
+                                    document.documentElement.clientWidth + "px",
+                                height:
+                                    document.documentElement.clientHeight +
+                                    "px",
+                            });
+                            let resizeTimer = null;
+                            $(window).on("resize", () => {
+                                if (resizeTimer != null) {
+                                    clearTimeout(resizeTimer);
+                                }
+                                resizeTimer = setTimeout(() => {
+                                    $("#pe-bg").css({
+                                        width:
+                                            document.documentElement
+                                                .clientWidth + "px",
+                                        height:
+                                            document.documentElement
+                                                .clientHeight + "px",
+                                    });
+                                }, 500);
+                            });
+                            $("#pe-close").click(function () {
+                                $("#pe-bg").remove();
+                            });
 
-                        $("#pe-fg")
-                            .find('input[type="checkbox"]')
-                            .each(function () {
-                                const $checkbox = $(this);
-                                const name = $checkbox.attr("name");
-                                GM.getValue(name, true).then((value) => {
-                                    $checkbox.prop("checked", value);
+                            $("#pe-fg")
+                                .find('input[type="checkbox"]')
+                                .each(function () {
+                                    const $checkbox = $(this);
+                                    const name = $checkbox.attr("name");
+                                    GM.getValue(name, true).then((value) => {
+                                        $checkbox.prop("checked", value);
+                                    });
+                                    $checkbox.on("change", () => {
+                                        const checked =
+                                            $checkbox.prop("checked");
+                                        $checkbox.prop(checked, checked);
+                                        GM.setValue(name, checked);
+                                        initConfig();
+                                    });
                                 });
-                                $checkbox.on("change", () => {
-                                    const checked = $checkbox.prop("checked");
-                                    $checkbox.prop(checked, checked);
-                                    GM.setValue(name, checked);
-                                    initConfig();
+                            $("#pe-fg")
+                                .find('input[type="text"]')
+                                .each(function () {
+                                    const $input = $(this);
+                                    const name = $input.attr("name");
+                                    GM.getValue(name).then((value) => {
+                                        $input.val(value);
+                                    });
+                                    $input.on("change", () => {
+                                        GM.setValue(name, $input.val());
+                                    });
                                 });
-                            });
-                        $("#pe-fg")
-                            .find('input[type="text"]')
-                            .each(function () {
-                                const $input = $(this);
-                                const name = $input.attr("name");
-                                GM.getValue(name).then((value) => {
-                                    $input.val(value);
-                                });
-                                $input.on("change", () => {
-                                    GM.setValue(name, $input.val());
-                                });
-                            });
-                    });
-                }
+                        })
+                    );
+
+                observer.disconnect();
+                break;
             }
         });
     })();
@@ -661,7 +667,9 @@ a[href="/premium/lead/lp/?g=anchor&i=work_detail_remove_ads"] {
                             );
 
                             $form.parent().after(
-                                $(`<button class="iahZRV">
+                                $(`<button title="${i18n(
+                                    "search_help_title"
+                                )}" class="iahZRV ekdKos">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill: currentColor; width: 20px;"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
 </button>`).on("click", () => {
                                     $("#pe-bg").remove();
@@ -1019,7 +1027,7 @@ a[href="/premium/lead/lp/?g=anchor&i=work_detail_remove_ads"] {
                     illust().pageCount === 1;
                 const selectorShareBtn = await GM.getValue(
                     GMkeys.selectorShareBtn,
-                    ".UXmvz"
+                    ".bTImJG"
                 ); // section 下的 div
 
                 // 热修复下载按钮的className
